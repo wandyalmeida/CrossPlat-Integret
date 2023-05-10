5640a3b4ba4e71644271d00d2ce1f8089de1709c
wandyalmeida <77290358+wandyalmeida@users.noreply.github.com>
2023-05-10 18:29:17 +0100

    Add xml file and update some function on script.js

--- javascript/script.js
+++ javascript/script.js
@@ -24,6 +24,7 @@ document.getElementById("navMenu").addEventListener("click", function() {
     }
     // Show selected tab
     document.getElementById(tabName).style.display = "block";
+    document.getElementById("songList").style.display = "none";
   }
 
   // show the first tab by default
@@ -42,5 +43,87 @@ document.getElementById("navMenu").addEventListener("click", function() {
     });
   });
 
+
+  // Função para adicionar uma música à lista de espera
+  function addToWaitingList(id, title, artist) {
+    // Criar um novo item de lista com o título e o artista da música
+    var li = document.createElement("li");
+    var text = document.createTextNode(id + " - " + title + " - " + artist);
+    li.appendChild(text);
+    
+    // Adicionar o item de lista à lista de espera
+    var ul = document.getElementById("waitingList");
+    ul.appendChild(li);
+    
+    // // Atualizar o arquivo XML com a nova música na lista de espera
+    // var xhttp = new XMLHttpRequest();
+    // xhttp.open("POST", "updateXml.php", true);
+    // xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    // xhttp.send("id="+ id + "title=" + title + "&artist=" + artist);
+  }
+
+
+function resetSongList() {
+  isSongListLoaded = false;
+  var list = document.getElementById("songList");
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+}
   
+function showInternacional(lista) {
+  resetSongList();
+
+  document.getElementById("playlist").style.display = "none";
+	document.getElementById("songList").style.display = "flex";
   
+  // A lista de músicas ainda não foi carregada, vamos carregá-la agora.
+  
+  // Carregar o arquivo XML
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      // Encontrar todas as músicas internacionais
+      var xmlDoc = this.responseXML;
+      var musics = xmlDoc.getElementsByTagName("musics");
+      var internationalSongs = null;
+      for (var i = 0; i < musics.length; i++) {
+        if (musics[i].getAttribute("name") == lista) {
+          internationalSongs = musics[i].getElementsByTagName("song");
+          break;
+        }
+      }
+      if (internationalSongs == null) {
+        var list = document.getElementById("songList");
+        var p = document.createElement("p");
+        p.innerHTML = "Nenhuma música internacional encontrada.";
+        list.appendChild(p);
+        console.log("Não há músicas internacionais no arquivo XML.");
+        return;
+      }
+      
+      var list = document.getElementById("songList");
+      
+      // Exibir cada música em uma linha da tabela
+      for (var i = 0; i < internationalSongs.length; i++) {
+        var title = internationalSongs[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+        var id = internationalSongs[i].getElementsByTagName("id")[0].childNodes[0].nodeValue;
+        var artist = internationalSongs[i].getElementsByTagName("artist")[0].childNodes[0].nodeValue;
+        var imgSrc = internationalSongs[i].getElementsByTagName("image")[0].childNodes[0].nodeValue;
+        
+        var li = document.createElement("li");
+        li.innerHTML = "<img src='" + imgSrc + "' alt='Imagem do CD'>" +
+                       "<div class='songInfo'>" +
+                       "<br> "+
+                       "<h2>" + title + "</h2>" +
+                       "<p>" + id + "</p>" +
+                       "<p>" + artist + "</p>" + 
+                       "<button class='redBackground ' onclick='addToWaitingList(\"" + id + "\", \"" + title + "\", \"" + artist + "\")'>+</button>" +
+                       "</div>";
+        list.appendChild(li);
+      }
+    }
+  };
+  xhttp.open("GET", "karaoke.xml", true);
+  xhttp.send();
+}
