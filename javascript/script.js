3bae6942007c33e2b2ecbb5bc9abc10988da442f
wandyalmeida <77290358+wandyalmeida@users.noreply.github.com>
2023-05-11 02:38:32 +0100

    ADD function to waitList

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,5 @@
+console.log("O código JavaScript foi carregado");
+
 
 // JavaScript
 document.getElementById("navMenu").addEventListener("click", function() {
@@ -43,24 +45,136 @@ document.getElementById("navMenu").addEventListener("click", function() {
     });
   });
 
+  function addToWaitingList(id, title, artist, userName) {
+    console.log(id, title, artist, userName);
+ 
+    // Criar uma nova linha de tabela com o título, artista e botão "Remover" da música
+    var tr = document.createElement("tr");
+    tr.setAttribute("data-id", id);
 
-  // Função para adicionar uma música à lista de espera
-  function addToWaitingList(id, title, artist) {
-    // Criar um novo item de lista com o título e o artista da música
-    var li = document.createElement("li");
-    var text = document.createTextNode(id + " - " + title + " - " + artist);
-    li.appendChild(text);
-    
-    // Adicionar o item de lista à lista de espera
-    var ul = document.getElementById("waitingList");
-    ul.appendChild(li);
-    
-    // // Atualizar o arquivo XML com a nova música na lista de espera
-    // var xhttp = new XMLHttpRequest();
-    // xhttp.open("POST", "updateXml.php", true);
-    // xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    // xhttp.send("id="+ id + "title=" + title + "&artist=" + artist);
+    var idCell = document.createElement("td");
+    idCell.textContent = id;
+    tr.appendChild(idCell);
+
+    var titleCell = document.createElement("td");
+    titleCell.textContent = title;
+    tr.appendChild(titleCell);
+
+    var artistCell = document.createElement("td");
+    artistCell.textContent = artist;
+    tr.appendChild(artistCell);
+
+    var userNameCell = document.createElement("td");
+    userNameCell.textContent = userName;
+    tr.appendChild(userNameCell);
+
+    var removeButton = document.createElement("button");
+    removeButton.innerHTML = "Remove";
+    removeButton.addEventListener("click", function() {
+      var itemId = this.parentNode.parentNode.getAttribute("data-id");
+      removeFromWaitingList(itemId);
+    });
+    var removeCell = document.createElement("td");
+    removeCell.appendChild(removeButton);
+    tr.appendChild(removeCell);
+      // Adicionar a linha de tabela à tabela
+      var table = document.getElementById("waitingList");
+      table.appendChild(tr);
+        var emptyMessage = document.getElementById("emptyMessage");
+        emptyMessage.style.display = "none";
+        document.getElementById("listas").style.display = "block";
+      
+        // Salvar a lista de espera no armazenamento local
+        var waitingList = getWaitingListFromPage();
+        console.log(waitingList);
+        saveToLocalStorage(waitingList);
   }
+  
+  function saveToLocalStorage(waitingList) {
+    console.log(waitingList);
+    localStorage.setItem("waitingList", JSON.stringify(waitingList));
+  }
+  
+  function getWaitingListFromPage() {
+    var waitingList = [];
+    var trs = document.querySelectorAll("#waitingList tr");
+    for (var i = 1; i < trs.length; i++) {
+      var tr = trs[i];
+      var id = tr.getAttribute("data-id");
+      var title = tr.cells[1].textContent;
+      var artist = tr.cells[2].textContent;
+      var userName = tr.cells[3].textContent;
+      waitingList.push({id: id, title: title, artist: artist, userName: userName});
+    }
+    return waitingList;
+  }
+
+  window.addEventListener("load", function() {
+    // console.log(localStorage.getItem("waitingList"));
+    // Recuperar a lista de espera do armazenamento local
+    var waitingList = loadFromLocalStorage();
+    console.log(waitingList);
+    
+    // Adicionar cada item da lista de espera à página
+    for (var i = 0; i < waitingList.length; i++) {
+      var item = waitingList[i];
+      console.log(item);
+      addToWaitingList(item.id, item.title, item.artist, item.userName);
+    }
+  });
+  
+  function loadFromLocalStorage() {
+    var waitingList = JSON.parse(localStorage.getItem("waitingList"));
+    if (waitingList === null) {
+      waitingList = [];
+    }
+    return waitingList;
+  }
+  
+  function removeFromWaitingList(id) {
+    // Encontrar o item da lista de espera com o ID correspondente
+    console.log("Função removeFromWaitingList chamada com o ID:", id);
+    var table = document.getElementById("waitingList");
+    var rows = Array.from(table.getElementsByTagName("tr"));
+    for (var i = 0; i < rows.length; i++) {
+      if (rows[i].getAttribute("data-id") == id) {
+        table.removeChild(rows[i]);
+        break;
+      }
+    }
+    // Mostrar a mensagem de lista vazia se a lista estiver vazia
+    if (table.getElementsByTagName("tr").length === 1) {
+      var emptyMessage = document.getElementById("emptyMessage");
+      emptyMessage.style.display = "block";
+      document.getElementById("listas").style.display = "none";
+    }
+    // Salvar a lista de espera atualizada no armazenamento local
+    var waitingList = getWaitingListFromPage();
+    saveToLocalStorage(waitingList);
+  }
+
+
+// Verificar se a página atual é a página da lista de espera
+if (document.getElementById("waitingList")) {
+  // Adicionar o botão "Limpar lista"
+  var clearButton = document.createElement("button");
+  clearButton.textContent = "Remove All";
+  clearButton.addEventListener("click", function() {
+    var table = document.getElementById("waitingList");
+    var rows = table.querySelectorAll("tr[data-id]");
+    for (var i = rows.length - 1; i >= 0; i--) {
+      table.removeChild(rows[i]);
+    }
+    // Mostrar a mensagem de lista vazia
+    var emptyMessage = document.getElementById("emptyMessage");
+    emptyMessage.style.display = "block";
+    document.getElementById("listas").style.display = "none";
+    // Salvar a lista de espera vazia no armazenamento local
+    saveToLocalStorage([]);
+  });
+  var div = document.getElementById("clearButtonDiv");
+  div.appendChild(clearButton);
+}
 
 
 function resetSongList() {
@@ -118,12 +232,31 @@ function showInternacional(lista) {
                        "<h2>" + title + "</h2>" +
                        "<p>" + id + "</p>" +
                        "<p>" + artist + "</p>" + 
-                       "<button class='redBackground ' onclick='addToWaitingList(\"" + id + "\", \"" + title + "\", \"" + artist + "\")'>+</button>" +
-                       "</div>";
+                       "<button class='redBackground' onclick='showUserNameInput(this, \"" + id + "\", \"" + title + "\", \"" + artist + "\")'>+</button>" +
+               "</div>";
         list.appendChild(li);
+        
       }
     }
   };
   xhttp.open("GET", "karaoke.xml", true);
   xhttp.send();
+}
+
+function showUserNameInput(button, id, title, artist) {
+  var userNameInput = document.createElement("input");
+  userNameInput.type = "text";
+  userNameInput.placeholder = "Your Name";
+  var addButton = document.createElement("button");
+  addButton.textContent = "+";
+  addButton.style.backgroundColor = 'red';
+  addButton.style.color = 'white';
+  addButton.addEventListener("click", function() {
+    addToWaitingList(id, title, artist, userNameInput.value);
+    alert('Music added')
+  });
+  var songInfoDiv = button.parentNode;
+  songInfoDiv.appendChild(userNameInput);
+  songInfoDiv.appendChild(addButton);
+  button.remove();
 }
